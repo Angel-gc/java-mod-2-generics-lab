ec784fb71592cf2a3019c0851f4559f31767c1e8
Angel Concepcion <angelconcepcion50@gmail.com>
2022-06-28 14:34:43 -0400

    Update StringRunner.java

--- src/main/java/StringRunner.java
+++ src/main/java/StringRunner.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 class Problem {
   // declare the convertToString method here
+  public static <T> String convertToString(T[] array){
+    System.out.println(Arrays.toString(array));
+    return Arrays.toString(array);
+  }
 }
 
 public class StringRunner {
@@ -7,8 +13,8 @@ public class StringRunner {
     Integer[] inputInt = { 1, 2, 3 };
     String[] inputStr = { "Cake", "Donut", "Cupcake" };
 
-    // System.out.println(Problem.convertToString(inputInt)); // [1, 2, 3]
-    // System.out.println(Problem.convertToString(inputStr)); // [Cake, Donut,
+    System.out.println(Problem.convertToString(inputInt)); // [1, 2, 3]
+    System.out.println(Problem.convertToString(inputStr)); // [Cake, Donut,
     // Cupcake]
   }
 }
